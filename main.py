86dcef31dc17b843c6f7f7a13f232b46ec31cfdb
Nishad Vyas <59637864+nishadvyas99@users.noreply.github.com>
2023-11-06 17:53:50 -0500

    Added chatbot file and functions

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from api import create_app
+from api.util.chatbot import set_api_key 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+set_api_key(os.getenv("OPENAI_API_KEY"))
 
 app = create_app()
 app.run()
