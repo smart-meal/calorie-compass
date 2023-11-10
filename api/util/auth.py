ee70ffc49513894645bda9f019dadfc809bd7e41
Artak Kirakosyan <artakkirakosyan96@mail.ru>
2023-11-10 17:13:28 -0500

    Add pipeline for style and test

--- api/util/auth.py
+++ api/util/auth.py
@@ -5,12 +5,11 @@ from flask import session, jsonify
 
 def require_session(func: Callable):
     def wrapper(*args, **kwargs):
-        if 'user_id' in session:
-            # Session data is valid, continue with the request
-            return func(*args, **kwargs)
-        else:
+        if 'user_id' not in session:
             # Session data is not valid, redirect to a login page or perform another action
             return jsonify({"error": "Unauthorized"}), 401
+        # Session data is valid, continue with the request
+        return func(*args, **kwargs)
 
     return wrapper
 
