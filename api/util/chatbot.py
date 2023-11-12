5665b767f113bc91d8fe4e58fd6b5cecddd6d68b
Nishad Vyas <59637864+nishadvyas99@users.noreply.github.com>
2023-11-12 12:34:59 -0500

    Checking and handling errors for openai api

--- api/util/chatbot.py
+++ api/util/chatbot.py
@@ -21,17 +21,33 @@ class Chat:
     def send_message(self, message: str, role: str = "user") -> str:
         new_message = {"role": role, "content": message}
         self.messages.append(new_message)
-        response = openai.ChatCompletion.create(
-            api_key=self.api_key,
-            model=self.model,
-            messages=self.messages,
-            max_tokens=self.max_tokens,
-        )
-
-        resp = response.choices[0].message
-        resp_message = resp.content
-        resp_role = resp.role
-        self.messages.append(
-            {"role": resp_role, "content": resp_message}
-        )
+        try:
+            response = openai.ChatCompletion.create(
+                api_key=self.api_key,
+                model=self.model,
+                messages=self.messages,
+                max_tokens=self.max_tokens,
+            )
+            resp = response.choices[0].message
+            resp_message = resp.content
+            resp_role = resp.role
+            self.messages.append(
+                {"role": resp_role, "content": resp_message}
+            )
+        except openai.error.OpenAIError as e:
+            if isinstance(e, openai.error.AuthenticationError):
+                print(f"OpenAI API request was not authorized: {e}")
+            elif isinstance(e, openai.error.PermissionError):
+                print(f"OpenAI API request was not permitted: {e}")
+            elif isinstance(e, openai.error.RateLimitError):
+                print(f"OpenAI API request exceeded rate limit: {e}")
+            else:
+                print(f"OpenAI API error: {e}")
+            resp_role = "assistant"
+            resp_message = None
+            self.messages.append(
+                {"role": resp_role, "content": resp_message}
+            )
         return resp_message
+    
+    
