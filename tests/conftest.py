3802eaf3746d46d7869f9e265c30e6039aeff61a
Artak Kirakosyan <artakkirakosyan96@mail.ru>
2023-11-12 12:17:57 -0500

    9 - Refactor due to convention issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,8 @@ from flask import Flask
 from mongoengine import connect
 
 
-@pytest.fixture(scope="module")
-def app():
+@pytest.fixture(scope="module", name="app")
+def fixture_app():
     a = Flask(__name__)
     a.config['SECRET_KEY'] = "secret"
     a.config['TESTING'] = True
@@ -18,12 +18,12 @@ def app():
         'host': 'mongodb://localhost',
     }
     connect('test_db', host='mongodb://localhost', mongo_client_class=mongomock.MongoClient)
-    from api import user
+    from api import user  # pylint: disable=import-outside-toplevel
     a.register_blueprint(user.user_blueprint)
 
     return a
 
 
-@pytest.fixture(scope="module")
-def client(app):
+@pytest.fixture(scope="module", name="client")
+def fixture_client(app):
     return app.test_client()
