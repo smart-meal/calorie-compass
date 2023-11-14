a0143ba1bba78807058df55b01d7803929c6e270
aditipingale <149647339+aditipingale@users.noreply.github.com>
2023-11-14 10:30:32 +0530

    Update schema.py
    Added validation to the user schema.

--- api/user/schema.py
+++ api/user/schema.py
@@ -14,6 +14,25 @@ class UserSchema(Schema):
     body_type = fields.Str(required=False)
     bmi = fields.Int(required=False)
 
+    @validates_schema
+    def validate_user(self, data, **kwargs):  # pylint: disable=unused-argument
+
+        if not re.search(r"[A-Za-z'-]", data["first_name"]):
+            raise ValidationError("Invalid character used in first name.")
+        if not re.search(r"[A-Za-z'-]", data["last_name"]):
+            raise ValidationError("Invalid character used in last name.")
+        if data["age"]<0:
+            raise ValidationError("Age can not be less than zero.")
+        if data["height"]<0:
+            raise ValidationError("Height can not be less than zero.")
+        if data["weight"]<0:
+            raise ValidationError("Weight can not be less than zero.")
+        if data["goal"].upper() == "MAINTAIN THE WEIGHT" or data["goal"].upper() == "LOSE WEIGHT" or data["goal"].upper() == "GAIN WEIGHT":
+            raise ValidationError("Invalid option choosen.")
+        if data["lifestyle"].upper() == "LAZY" or data["lifestyle"].upper() == "SEDENTARY" or data["lifestyle"].upper() == "ACTIVE" or data["lifestyle"] == "MODERATE":
+            raise ValidationError("Invalid option choosen.")
+          
+
 class LoginSchema(Schema):
     username = fields.Str(required=True)
     password = fields.Str(required=True)
