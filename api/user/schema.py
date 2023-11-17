01fff46c84d8c9bf60776931c9e75e4316f81b19
jyu236 <147775095+jyu236@users.noreply.github.com>
2023-11-16 21:32:30 -0800

    18 create endpoints to update password (#38)
    * add update password endpoint
    
    * add test

--- api/user/schema.py
+++ api/user/schema.py
@@ -33,3 +33,28 @@ class RegisterSchema(Schema):
 
         if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", data["password"]):
             raise ValidationError("Password must contain at least one special character")
+
+class UpdatePasswordSchema(Schema):
+    old_password = fields.Str(required=True)
+    new_password = fields.Str(required=True)
+    repeat_password = fields.Str(required=True)
+
+    @validates_schema
+    def validate_numbers(self, data, **kwargs):  # pylint: disable=unused-argument
+        if data["new_password"] != data["repeat_password"]:
+            raise ValidationError("Password do not match")
+
+        if len(data["new_password"]) < 8:
+            raise ValidationError("Password must be at least 8 characters long")
+
+        if not re.search(r"[A-Z]", data["new_password"]):
+            raise ValidationError("Password must contain at least one uppercase letter")
+
+        if not re.search(r"[a-z]", data["new_password"]):
+            raise ValidationError("Password must contain at least one lowercase letter")
+
+        if not re.search(r"\d", data["new_password"]):
+            raise ValidationError("Password must contain at least one digit")
+
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", data["new_password"]):
+            raise ValidationError("Password must contain at least one special character")
